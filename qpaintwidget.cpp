d64af73a2104b7d43f3dd4a594f5ebe38c495216
aminbenarieb <amin.benarieb@gmail.com>
2016-03-28 23:00:08 +0300

    Implemented drawing workflow & DDA algorithm.

--- qpaintwidget.cpp
+++ qpaintwidget.cpp
@@ -7,12 +7,82 @@
 QPaintWidget::QPaintWidget(QWidget * parent) : QWidget(parent)
 {
   // Initialization
+    method = -1;
 }
 
 void QPaintWidget::paintEvent(QPaintEvent *) {
 
+
     QPainter ppainter(this);
-    ppainter.setPen(QPen(Qt::blue, kDrawPenWidth));
-    ppainter.setBrush(QBrush(Qt::transparent));
+    if (spectr)
+    {
+        drawSpectr(&ppainter, p1, lineCount, color);
+    }
+    else
+    {
+        switch (method) {
+        case 0:
+            drawDDALine(&ppainter, p1, p2, color);
+            break;
+        case 1:
+            drawBrezenhamIntLine(&ppainter, p1, p2, color);
+            break;
+        case 2:
+            drawBrezenhamFloatLine(&ppainter, p1, p2, color);
+            break;
+        case 3:
+            drawBrezenhamSmoothLine(&ppainter, p1, p2, color);
+            break;
+        case 4:
+            drawQtLine(&ppainter, p1, p2, color);
+            break;
+        default:
+            break;
+        }
+    }
 
 }
+
+
+// Algorithms with drawing
+
+void QPaintWidget::drawDDALine(QPainter *ppainter, QPoint p1, QPoint p2, QColor color){
+
+    // Input data
+    int x1 = p1.x();
+    int y1 = p1.y();
+    int x2 = p2.x();
+    int y2 = p2.y();
+
+    // Intermediate calculation
+    double x = x1, y = y1;
+    int dx = abs(x2-x1);
+    int dy = abs(y2-y1);
+    int l = dx > dy ? dx : dy;
+    dx /= l;
+    dy /= l;
+
+    // Output drawing
+    ppainter->save();
+    ppainter->setPen(QPen(color, kDrawPenWidth));
+    for (int i = 0; i < l; i++)
+    {
+       ppainter->drawPoint(round(x), round(y));
+       x += dx;
+       y += dy;
+    }
+     ppainter->restore();
+
+}
+void QPaintWidget::drawBrezenhamIntLine(QPainter *ppainter, QPoint p1, QPoint p2, QColor color){}
+void QPaintWidget::drawBrezenhamFloatLine(QPainter *ppainter, QPoint p1, QPoint p2, QColor color){}
+void QPaintWidget::drawBrezenhamSmoothLine(QPainter *ppainter, QPoint p1, QPoint p2, QColor color){}
+void QPaintWidget::drawQtLine(QPainter *ppainter, QPoint p1, QPoint p2, QColor color){
+
+    ppainter->save();
+    ppainter->setPen(QPen(color, kDrawPenWidth));
+    ppainter->drawLine(p1,p2);
+    ppainter->restore();
+
+}
+void QPaintWidget::drawSpectr(QPainter *ppainter, QPoint p1,  int lineCount, QColor color){}
--- qpaintwidget.h
+++ qpaintwidget.h
@@ -11,9 +11,26 @@ class QPaintWidget : public QWidget
     Q_OBJECT
 public:
     QPaintWidget(QWidget * parent = 0);
+    bool spectr;
+    int method;
+    int lineCount;
+    QPoint p1;
+    QPoint p2;
+    QColor color;
 
 protected:
     void paintEvent(QPaintEvent *);
+
+private:
+    void drawDDALine(QPainter *,QPoint, QPoint, QColor);
+    void drawBrezenhamIntLine(QPainter *, QPoint, QPoint, QColor);
+    void drawBrezenhamFloatLine(QPainter *, QPoint, QPoint, QColor);
+    void drawBrezenhamSmoothLine(QPainter *, QPoint, QPoint, QColor);
+    void drawQtLine(QPainter *, QPoint, QPoint, QColor);
+    void drawSpectr(QPainter *, QPoint, int, QColor);
+
+
+
 };
 
 #endif // QPAINTWIDGET_H
